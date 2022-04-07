f0e3baa0a159c2fbae0dc376b6121ab9ad332a42
akimajid <akimajid07@gmail.com>
2022-04-07 15:00:12 +0700

    added create a rooms for users

--- src/pages/rooms.jsx
+++ src/pages/rooms.jsx
@@ -1,48 +1,113 @@
-import { Container, Label } from "reactstrap"
-import axios from "axios"
-import { useState, useEffect } from "react"
-import ReactSelect from "react-select"
+import { Container, Label, Button, Table } from "reactstrap";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import ReactSelect from "react-select";
+import { io } from "socket.io-client"
+
+const socketConnection = io("http://localhost:2000")
 
 const RoomsPage = () => {
-    const [users, setUsers] = useState([])
-    
+  const [users, setUsers] = useState([]);
+  const [selectedUsers, setSelectedUSers] = useState([]);
 
-    const fetchUsers  = async () => {
-      try {
-        const resUsers = await axios.get("http://localhost:2000/users")
-  
-        setUsers(resUsers.data.result)
-      } catch (err) {
-        console.log(err)
-      }
+  const [rooms, setRooms] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const resUsers = await axios.get("http://localhost:2000/users");
+
+      setUsers(resUsers.data.result);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const usersReactSelectOptions = () => {
-        return users.map((val) => {
-            return {
-                label: val.username,
-                value: val.id
-            }
-        })
+  const usersReactSelectOptions = () => {
+    return users.map((val) => {
+      return {
+        label: val.username,
+        value: val.id,
+      };
+    });
+  };
+
+  const fetchRooms = async () => {
+    try {
+      const roomsRes = await axios.get("http://localhost:2000/chat/rooms");
+
+      setRooms(roomsRes.data.result);
+    } catch (err) {
+      console.log(err);
     }
-  
-    useEffect(() => {
-        fetchUsers()
-    }, [])
+  };
 
+  const createRoomHandler = async () => {
+    try {
+      await axios.post("http://localhost:2000/chat/rooms", {
+        userIds: selectedUsers.map((val) => val.value),
+      });
 
-    return (
-        <Container>
-            <div className="row">
-                <div className="col-6">
-                    <h2>Create Chat Room</h2>
-                    <Label>Users</Label>
-                    <ReactSelect isMulti options={usersReactSelectOptions()} />
-                </div>
-                <div className="col-6"></div>
-            </div>
-        </Container>
-    )
-}
+      fetchRooms();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-export default RoomsPage
+  useEffect(() => {
+    fetchUsers();
+    fetchRooms();
+
+    socketConnection.on("NEW_ROOM_CREATED", (data) => {
+        setRooms(data)
+    })
+
+    return () => {
+        socketConnection.disconnect()
+    }
+  }, []);
+
+  return (
+    <Container>
+      <div className="row">
+        <div className="col-6">
+          <h2>Create Chat Room</h2>
+          <Label>Users</Label>
+          <ReactSelect
+            onChange={(values) => setSelectedUSers(values)}
+            isMulti
+            options={usersReactSelectOptions()}
+          />
+
+          <Button className="mt-3" onClick={createRoomHandler}>
+            Create Room
+          </Button>
+
+          <h2 className="mt-3">Avaiable Rooms</h2>
+          <Table>
+            <thead>
+              <tr>
+                <td>Room Name</td>
+                <td>Participants</td>
+              </tr>
+            </thead>
+            <tbody>
+              {rooms.map((val) => {
+                return (
+                  <tr>
+                    <td>{val.room_name}</td>
+                    <td>
+                      {val?.Users?.map((user) => user.username).join(", ")}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+        <div className="col-6"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default RoomsPage;
